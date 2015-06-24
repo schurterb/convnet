123c03028e8e8c752bd01047522ec8d6b5cc3780
schurterb <schurter.brandon@gmail.com>
2015-06-24 11:35:12 -0400

    Commented and reorganized cnn_test.py

--- cnn_test.py
+++ cnn_test.py
@@ -11,24 +11,53 @@ from cnn import CNN
 from trainer import Trainer
 from load_data import open_data, close_data
 
-
-
-
 #Load the data for training
 data_folder = 'nobackup/turaga/data/fibsem_medulla_7col/trvol-250-1-h5/'
 data_file = 'img_normalized.h5'
 label_file = 'groundtruth_aff.h5'
 train_data, train_label, files = open_data(data_folder, data_file, label_file)
 
+#Number of layers in the network
+num_layers = 5
+#Number of filters per layer (constant across the network)
+num_filters = 6
+#Size of filters in each layer (constant across the network)
+# note - this only one side of each filter, which are assumed to be cubic
+filter_size = 5
+#Size of mini-batches for training
+batch_size = 10
+#Activation function for the first num_layers-1 layers of the network
+#The last layer is always sigmoid, since the goal is to classify neighboring
+# pixels as connected or not, producing an affinity graph
+activation = 'relu'
+#Cost function to evaluate the loss of the network, by default this is the MSE
+#cost_func = 'MSE'
 
+#------------------------------------------------------------------------------
 #Create the network to train
-network = CNN(num_layers = 3, num_filters = 3, filter_size = 3, batch_size = 4, activation = 'relu', cost_func = 'MSE')
+network = CNN(num_layers = num_layers, num_filters = num_filters, 
+              filter_size = filter_size, batch_size = batch_size,
+              activation = activation)
 
+#Learning Methods include standard SGD, RMSprop, and ADAM
+learning_method = 'RMSprop'
+#The hyper-parameters for the chosen learning method must also be provided
+learning_rate = 0.0001
+decay_rate = 0.98
+damping = 1.0e-8
+
+#------------------------------------------------------------------------------
 #Create a trainer for the network
-network_trainer = Trainer(network.get_network(), batch_size = 4, learning_rate = 0.001)
+network_trainer = Trainer(network.get_network(), batch_size = batch_size,
+                          learning_rate = learning_rate, decay_rate = decay_rate,
+                          damping = damping)
 
+#Number of updates to train over
+updates = 1000
+
+#------------------------------------------------------------------------------
 #Train the network
-network_trainer.train(train_data, train_label, 10)
+train_error = network_trainer.train(train_data, train_label, duration = updates)
 
 #After training, close the data_files
 close_data(files)
