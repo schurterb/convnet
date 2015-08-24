b8567899a3c8a27a5c70e04e20ef395e8c3e51fa
schurterb <schurter.brandon@gmail.com>
2015-08-24 09:09:21 -0400

    best malis trainer setup

--- net_analysis.py
+++ net_analysis.py
@@ -29,10 +29,8 @@ label_file = 'groundtruth_aff.h5'
 seg_file = 'groundtruth_seg.h5'
 
 
-#res_files = ('ADAM', 'ADAM_1', 'ADAM_2')#, 'ADAM_3')
-#res_files = ('rmsp_1', 'rmsp_2')#, 'rmsp_3')
-#res_files = ("results/deep_nets_1/ADAM", "results/learn_opt_res1/name='RMSprop'_lr=0.0001_b1=0.9_b2=0_dp=1e-08", "results/deep_nets_2/rmsp_1")
-res_files = ('conv-5.6.5', 'backups/conv-5.6.5', 'conv-5.30.5', 'backups/conv-5.30.5')#, 'conv-10.20.5', 'conv-8.25.5', 'conv-12.11.5')
+res_files = ('conv-5.6.5', 'conv-5.30.5')#, 'conv-10.20.5', 'conv-8.25.5', 'conv-12.11.5')
+#res_files = ('conv-5.6.5', 'conv-5.30.5', 'conv-8.25.5', 'conv-8.50.5', 'conv-9.20.5', 'conv-10.20.5', 'conv-12.11.5')
 #------------------------------------------------------------------------------ 
 
 #Load the data for testing
@@ -63,7 +61,7 @@ print 'Analysis complete'
 results.learning(10)
 
 ###Display the performance metrics
-results.performance()
+#results.performance()
 #
 ##Close test data
 ##test_data.close()
